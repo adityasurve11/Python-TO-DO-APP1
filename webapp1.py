2eadb039ab1540086966f47a6b8ef4d72fc22484
ADITYA <aditya11surve08@gmail.com>
2024-09-10 20:36:53 +0530

    Implement "Add new todos" feature

--- webapp1.py
+++ webapp1.py
@@ -3,6 +3,11 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
 st.title("My Todos App")
 st.subheader("This is my todo app.")
 st.write("This app is to remind me of tasks and increase productivity")
@@ -10,7 +15,9 @@ st.write("This app is to remind me of tasks and increase productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label = '', placeholder="Add new todo...")
+st.text_input(label = '', placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 print("Hello")
 
+st.session_state
